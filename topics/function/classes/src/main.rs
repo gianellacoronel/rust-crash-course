fbe862c3636974494a13681f014347964f589d07
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-11-27 17:18:43 -0500

    function class

---
+++ topics/function/classes/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused)]
+
+//To return more than one value
+// fn add(x: u32, y: u32) -> (u32, bool){
+//     return (x + y, true);
+// }
+
+fn add_with_return(x: u32, y: u32) -> u32{
+    return x + y;
+}
+
+fn add(x: u32, y: u32) -> u32{
+    x + y //here we don't use "return" and also remove the semicolon, but it still returns that value
+}
+
+fn print(s: String){
+    println!("{s}{s}{s}{s}{s}");
+}
+
+fn main(){
+    let x = 1;
+    let y = 2;
+    let z = add(x, y);
+    println!("{x} + {y} = {z}");
+
+    print("🌸".to_string());
+}
